6a9f81ed9f3fb8b46fc9bce555c9f2c2b6c7fc2a
mrojash97 <49731172+mrojash97@users.noreply.github.com>
2025-09-30 09:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function efectoHabilidades(){
     }
 
 const pdfUrls = {
-      1: "https://drive.google.com/file/d/1kF5LZGtVYvwxwcotz79YzIaihtOvS_Hw/preview",
+      1: "https://drive.google.com/file/d/14vo4hszrNMi-ATR2i_ZSQRly-L0tm76a/preview",
       2: "https://drive.google.com/file/d/1ldKBaGbmOgzuPMddeoJkTSxo7oImBdpE/preview",
       3: "https://drive.google.com/file/d/1Sl8xGgrzBgoNA34_19Po_jEw0ulFIJZl/preview",
       4: "https://drive.google.com/file/d/1fTYqGJOZ9UIppNCDQksKuUz75OmLyQc2/preview",
